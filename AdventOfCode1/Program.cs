840ab9eeab764ab570327d62dfefa8c0b63e630f
Alex Nguyen <alexandermusic77@gmail.com>
2023-12-01 20:47:29 +0000

    solution

---
+++ AdventOfCode1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            int result = 0;
+            foreach (string line in input)
+            {
+                string val = Regex.Replace(line, "[A-Za-z ]", "");
+                val = $"{val[0]}{val[val.Length-1]}";
+                Console.WriteLine(val);
+                result += int.Parse(val);
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
